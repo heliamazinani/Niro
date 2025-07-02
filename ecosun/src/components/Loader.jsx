94c2209c615ad00d6fc1c46b123f9d63c2c643fe
HeliaMazinani <13helia1383@gmail.com>
2025-07-02 19:24:27 +0430

    first commit

---
+++ ecosun/src/components/Loader.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <>
+      {/* Loader Wrap */}
+      <div className="loader-wrap">
+        <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+          <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+        </svg>
+
+        <div className="loader-wrap-heading">
+          <div className="load-text">
+            <span>در حال آماده سازی</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Cursor (optional, for custom cursor effect) */}
+      <div className="cursor"></div>
+
+      {/* Scroll Progress Circle */}
+      <div className="progress-wrap cursor-pointer">
+        <svg
+          className="progress-circle svg-content"
+          width="100%"
+          height="100%"
+          viewBox="-1 -1 102 102"
+        >
+          <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
